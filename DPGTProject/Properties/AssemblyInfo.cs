a2a812d40d2b6cd3f31c67aa46cc200a85b701b6
maksgranko <42186873+maksgranko@users.noreply.github.com>
2025-04-28 21:08:10 +0300

    2: Register Fix

--- DPGTProject/Properties/AssemblyInfo.cs
+++ DPGTProject/Properties/AssemblyInfo.cs
@@ -22,8 +22,8 @@ using System.Runtime.InteropServices;
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("45a23ed4-3255-43e8-8302-12f7e5d50ea7")]
 
-[assembly: AssemblyVersion("1.2025.118.288")]
-[assembly: AssemblyFileVersion("1.2025.118.288")]
+[assembly: AssemblyVersion("1.2025.118.298")]
+[assembly: AssemblyFileVersion("1.2025.118.298")]
 
 
-[assembly: AssemblyInformationalVersion("0.2025.118.179")]
+[assembly: AssemblyInformationalVersion("0.2025.118.189")]
