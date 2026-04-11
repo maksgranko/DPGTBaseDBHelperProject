27e3c997120b64574bcd65633408214823f6c699
Maks Granko <maksgranko@gmail.com>
2026-04-11 16:19:22 +0300

    Unicode Fixes

--- DPGTProject/Configs/SystemConfig.cs
+++ DPGTProject/Configs/SystemConfig.cs
@@ -29,32 +29,32 @@ namespace DPGTProject
         public static bool additionalButtonsInTables = true;                                                            // Добавить кнопки добавления и изменения
         public static bool exportRightInTables = false;                                                                 // Добавить прямой экспорт
         public static bool helpButtonInTables = true;                                                                   // Добавить кнопку помощи
-        public static bool enableFilterInTables = true;                                                                // Включить фильтр
-        public static bool enableSearchInTables = false;                                                                 // Включить поиск
+        public static bool enableFilterInTables = true;                                                                 // Включить фильтр
+        public static bool enableSearchInTables = false;                                                                // Включить поиск
         public static bool enableSortingInTables = true;                                                                // Включить сортировку
         #endregion +++ UniversalTableViewerForm функции +++                                 
 
         #region +++  RegisterForm функции +++
-        public static bool addRolesWhenRegistering = false;                                                            // Добавить выбор роли при регистрации
+        public static bool addRolesWhenRegistering = false;                                                             // Добавить выбор роли при регистрации
         #endregion +++ RegisterForm функции +++
         #region +++ Auth / схема БД +++
-        public static bool authHashPasswords = true;                                                                   // Хэшировать пароли при регистрации/авторизации
-        public static DatabaseGenerationMode databaseGenerationMode = DatabaseGenerationMode.Simple;                   // Режим генерации БД: None/Simple/Standard/Full
+        public static bool authHashPasswords = true;                                                                    // Хэшировать пароли при регистрации/авторизации
+        public static DatabaseGenerationMode databaseGenerationMode = DatabaseGenerationMode.Simple;                    // Режим генерации БД: None/Simple/Standard/Full
         #endregion +++ Auth / схема БД +++
 
 
         #endregion --- Дополнительные функции ---
 
         #region --- Роли, необходимые для программы ---
-        public static string[] roles = new string[] { "Администратор", "Менеджер" };                                   // Здесь прописываются роли! Вы можете добавить свою.
-                                                                                                                       // Роли добавляются в список с начала в конец. По умолчанию при отключённом выборе, выдаётся последняя роль.
+        public static string[] roles = new string[] { "Администратор", "Менеджер" };                                    // Здесь прописываются роли! Вы можете добавить свою.
+                                                                                                                        // Роли добавляются в список с начала в конец. По умолчанию при отключённом выборе, выдаётся последняя роль.
 
         #region +++ Права, индивидуальные к каждой РОЛИ ПО УМОЛЧАНИЮ +++
         public static Dictionary<string, PermissionFlags> DefaultRolePermissions = new Dictionary<string, PermissionFlags>()
         {
-            ["default"] = PermissionFlags.None,                                                                        // default - права для ВСЕХ
+            ["default"] = PermissionFlags.None,                                                                         // default - права для ВСЕХ
             ["Администратор"] = PermissionFlags.All,
-            ["Менеджер"] = PermissionFlags.Read | PermissionFlags.Write | PermissionFlags.Delete
+            ["Менеджер"] = PermissionFlags.ReadWrite | PermissionFlags.Delete
         };
         #endregion +++ Права, индивидуальные к каждой РОЛИ ПО УМОЛЧАНИЮ +++
         // Порядок прав: <-эти^ права важнее, чем те, что ниже, они работают глобально ко всем таблицам
@@ -77,11 +77,11 @@ namespace DPGTProject
         #endregion --- Роли, необходимые для программы ---
 
         #region --- Таблицы и автоопределение таблиц ---
-        public static string[] tables = new string[] { };                                                               // Пример заполнения: tables = new string[] { "Documents", "DocumentHistory", "Fines", "Owners", "Violations" }; (!) НЕОБХОДИМО ОТКЛЮЧИТЬ АВТООПРЕДЕЛЕНИЕ ДЛЯ КОРРЕКТНОЙ РАБОТЫ!
-        public static string[] virtualTables = new string[] { };                                                        // Если необходимо создать виртуальную таблицу со своей логикой работы
-        public static string[] removeFromTableWhenStart = new string[] { };  // (!) мб стало бесполезным из-за ролей    // Какие таблицы удалять, после запуска(из добавленных вручную или автоматически добавленных)
-        public static bool tableAutodetect = true;                                                                      // Включить автоопределение таблиц из базы данных
-        public static string[] removeFromTableWhenAutodetect = new string[] { };                                        // (!) Какие таблицы удалять, после автоматического определения (!) Не работает если отключён автодетект.
+        public static string[] tables = new string[] { };                                                                // Пример заполнения: tables = new string[] { "Documents", "DocumentHistory", "Fines", "Owners", "Violations" }; (!) НЕОБХОДИМО ОТКЛЮЧИТЬ АВТООПРЕДЕЛЕНИЕ ДЛЯ КОРРЕКТНОЙ РАБОТЫ!
+        public static string[] virtualTables = new string[] { };                                                         // Если необходимо создать виртуальную таблицу со своей логикой работы
+        public static string[] removeFromTableWhenStart = new string[] { };  // (!) мб стало бесполезным из-за ролей     // Какие таблицы удалять, после запуска(из добавленных вручную или автоматически добавленных)
+        public static bool tableAutodetect = true;                                                                       // Включить автоопределение таблиц из базы данных
+        public static string[] removeFromTableWhenAutodetect = new string[] { };                                         // (!) Какие таблицы удалять, после автоматического определения (!) Не работает если отключён автодетект.
         #endregion --- Таблицы и автоопределение таблиц ---
 
         #region --- Цветовая тема и иконка ---
--- DPGTProject/Properties/AssemblyInfo.cs
+++ DPGTProject/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("45a23ed4-3255-43e8-8302-12f7e5d50ea7")]
 
-[assembly: AssemblyVersion("1.2025.323.327")]
-[assembly: AssemblyFileVersion("1.2025.323.327")]
+[assembly: AssemblyVersion("1.2026.101.328")]
+[assembly: AssemblyFileVersion("1.2026.101.328")]
 
-[assembly: AssemblyInformationalVersion("0.2025.323.218")]
+[assembly: AssemblyInformationalVersion("0.2026.101.219")]
